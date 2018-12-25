b5ea7a5e85cf60467012ff0e5c1552c89d26947c
oscardoudou <oscar9591@icloud.com>
2018-12-24 20:44:18 -0500

    Initial Commit

---
+++ Link It/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Link It
+//
+//  Created by 张壹弛 on 12/24/18.
+//  Copyright © 2018 张壹弛. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Link It/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Link It
+//
+//  Created by 张壹弛 on 12/24/18.
+//  Copyright © 2018 张壹弛. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
